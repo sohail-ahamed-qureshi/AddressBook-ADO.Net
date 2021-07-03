235a83c9809ee5b03a1eaa22fee465ccbe0ebf2b
sohail <sohailqureshi82@gmail.com>
2021-07-03 12:08:50 +0530

    [sohail] Add: ability to create table for addressbook

--- AddressBookADO.Net/DatabaseQuery.sql
+++ AddressBookADO.Net/DatabaseQuery.sql
@@ -1,2 +1,15 @@
-﻿create database AddressBookADO;
+﻿--UC1 ability to create Database
+create database AddressBookADO;
 use AddressBookADO;
+
+--UC2 -- ability to create table
+create table ContactBook(
+firstName varchar(255),
+lastName varchar(255),
+address varchar(255),
+city varchar(255),
+state varchar(255),
+zip int,
+phoneNumber bigint,
+email varchar(255)
+);
