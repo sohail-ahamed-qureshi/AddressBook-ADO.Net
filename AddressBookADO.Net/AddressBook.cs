f446e4c17e38e19cb9127e81e374f76b14ccd31a
sohail <sohailqureshi82@gmail.com>
2021-07-03 11:59:56 +0530

    [sohail] Add: ability to create database

---
+++ AddressBookADO.Net/AddressBook.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System;
+
+namespace AddressBookDatabase
+{
+    class AddressBook
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipcode { get; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+
+    }
+}
---
+++ AddressBookADO.Net/DatabaseQuery.sql
@@ -0,0 +1,2 @@
+﻿create database AddressBookADO;
+use AddressBookADO;
